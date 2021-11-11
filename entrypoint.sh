1d6591c2e005b16e159ff242216f0b7e1caf3542
nuonuonuonuoyan <nuonuonuonuoyan@github.com>
2021-11-11 09:27:07 +0800

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,11 +56,11 @@ if [ $? -eq 0 ]
 then
    echo "项目已成功拷贝到服务器相应目录下"
    echo "开始连接远程服务器..."
-   if [ test $INPUT_PASSWORD && test $INPUT_PASSWORD == "" ]
-   then
-     sh -c "ssh -i $ID_RSA -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/command.sh"
+   if [ $INPUT_KEY != "" ]
+   then 
+      sh -c "ssh -i $ID_RSA -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/command.sh"
    else
-     sh -c "sshpass -p $INPUT_PASSWORD ssh -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/command.sh"
+      sh -c "sshpass -p $INPUT_PASSWORD ssh -o StrictHostKeyChecking=no -p $INPUT_PORT ${INPUT_USER}@${INPUT_HOST} < $HOME/command.sh"
    fi
 fi 
 
